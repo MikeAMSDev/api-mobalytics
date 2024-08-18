7a20cbf91b7b0bf0a7dc31d429e846cbc36fdd6b
Mikeleto <mikeletorsayayin@gmail.com>
2024-08-18 13:04:15 +0200

    or so I thought

--- database/migrations/2024_08_17_091235_update_type_object_enum_values.php
+++ database/migrations/2024_08_17_091235_update_type_object_enum_values.php
@@ -9,15 +9,37 @@ class UpdateTypeObjectEnumValues extends Migration
 {
     public function up()
     {
-        DB::statement("ALTER TABLE items MODIFY COLUMN type_object ENUM('Basic', 'Combined', 'Faerie', 'Radiant', 'Non-Craftable', 'Consumable', 'Support', 'Artifact') NOT NULL");
+        $connection = config('database.default');
 
-        DB::statement("UPDATE items SET type_object = 'Non-Craftable' WHERE type_object = 'Non-crafteable'");
+        if ($connection === 'pgsql') {
+            DB::statement("ALTER TABLE items ALTER COLUMN type_object TYPE VARCHAR(255)");
+
+            DB::statement("ALTER TABLE items ADD CONSTRAINT check_type_object CHECK (type_object IN ('Basic', 'Combined', 'Faerie', 'Radiant', 'Non-Craftable', 'Consumable', 'Support', 'Artifact'))");
+
+            DB::statement("UPDATE items SET type_object = 'Non-Craftable' WHERE type_object = 'Non-crafteable'");
+        } elseif ($connection === 'mysql') {
+            DB::statement("ALTER TABLE items MODIFY COLUMN type_object ENUM('Basic', 'Combined', 'Faerie', 'Radiant', 'Non-Craftable', 'Consumable', 'Support', 'Artifact') NOT NULL");
+
+            DB::statement("UPDATE items SET type_object = 'Non-Craftable' WHERE type_object = 'Non-crafteable'");
+        } else {
+            throw new Exception("Database connection [$connection] not supported.");
+        }
     }
 
     public function down()
     {
-        DB::statement("ALTER TABLE items MODIFY COLUMN type_object ENUM('Basic', 'Combined', 'Faerie', 'Radiant', 'Non-crafteable', 'Consumable', 'Support', 'Artifact') NOT NULL");
+        $connection = config('database.default');
 
-        DB::statement("UPDATE items SET type_object = 'Non-crafteable' WHERE type_object = 'Non-Craftable'");
+        if ($connection === 'pgsql') {
+            DB::statement("ALTER TABLE items DROP CONSTRAINT check_type_object");
+
+            DB::statement("UPDATE items SET type_object = 'Non-crafteable' WHERE type_object = 'Non-Craftable'");
+        } elseif ($connection === 'mysql') {
+            DB::statement("ALTER TABLE items MODIFY COLUMN type_object ENUM('Basic', 'Combined', 'Faerie', 'Radiant', 'Non-crafteable', 'Consumable', 'Support', 'Artifact') NOT NULL");
+
+            DB::statement("UPDATE items SET type_object = 'Non-crafteable' WHERE type_object = 'Non-Craftable'");
+        } else {
+            throw new Exception("Database connection [$connection] not supported.");
+        }
     }
 }
